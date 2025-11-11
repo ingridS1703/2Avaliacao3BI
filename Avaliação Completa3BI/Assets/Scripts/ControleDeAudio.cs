e8c22a235e6def0fb02667bdb05f9f72151d9e20
ingridS1703 <i.sayonara@escolar.ifrn.edu.br>
2025-11-11 11:54:15 -0300

    ....
    painel de audio configurado

--- Avaliação Completa3BI/Assets/Scripts/ControleDeAudio.cs
+++ Avaliação Completa3BI/Assets/Scripts/ControleDeAudio.cs
@@ -1,16 +1,46 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class ControleDeAudio : MonoBehaviour
+
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int volume;
+    public int volumeSFX;
+    public bool musica;
+    
+    public Slider volumeSlider;
+    public Slider volumeSFXSlider; 
+    public Toggle toggleMusica;
+   public TMP_Text textoMusica;
+   
+   
     void Start()
     {
-        
+        musica = toggleMusica.isOn;
+        volume = (int)volumeSlider.value;
+        volumeSFX = (int)volumeSFXSlider.value;
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        musica = toggleMusica.isOn;
+        volume = (int)volumeSlider.value;
+        volumeSFX = (int)volumeSFXSlider.value;
+
+    
+        if (musica == true)
+        {
+            textoMusica.text = "Ligado";
+            textoMusica.color = Color.green;
+        }
+        else
+        {
+            textoMusica.text = "Desligado";
+            textoMusica.color = Color.red;
+        }
     }
 }
